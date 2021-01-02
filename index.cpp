c67647469bf5653a7839814efbfe5b6c82556ca3
Aadarsh <aadarsh-s@outlook.com>
2021-01-02 12:27:06 +0530

    Basic vector operation

--- index.cpp
+++ index.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello Mofos!";
+    vector<int> sno(2);
+    sno[1] = 1;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << sno[i] << endl;
+    }
     return 0;
 }
