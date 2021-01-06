f75209e48dc18d2cca97b6bc61ff195396c4bd60
Aadarsh <aadarsh-s@outlook.com>
2021-01-06 11:19:06 +0530

    Basic pointers

---
+++ pointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a{10};
+    int *p{&a};
+    cout << a << endl;
+    cout << &a << endl;
+    cout << p << endl;
+    cout << *p << endl;
+    *p = 20;
+    cout << a << endl;
+}
